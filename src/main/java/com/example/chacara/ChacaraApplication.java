5c5ac3c8475f7a5b38f0da16d5b26b3fb269d2de
Luisfelippedev <felippe.luisdev@gmail.com>
2024-06-25 14:42:09 -0300

    🎉 initial setup project

---
+++ src/main/java/com/example/chacara/ChacaraApplication.java
@@ -0,0 +1,13 @@
+package com.example.chacara;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChacaraApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ChacaraApplication.class, args);
+	}
+
+}
